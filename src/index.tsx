cdcf690f10dd02f32beda9d33d3130e2da941e59
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-08-04 00:06:21 +0200

    Add provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,24 @@ import './sass/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {applyMiddleware, createStore, Store} from "redux"
+import {Provider} from "react-redux"
+import thunk from "redux-thunk"
+import reducer from "./store/reducer";
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const store: Store<ArticleState, ArticleAction> & {
+    dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
