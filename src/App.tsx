bee9657e2fdb44da6c59c458db8649d01ac8298c
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-08-04 00:47:42 +0200

    Add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
 import './sass/App.scss';
+import {Dispatch} from "redux";
+import {AddArticle} from "./sass/components/AddArticle";
+import { addArticle} from "./store/actionCreators";
+import {removeArticle} from "./store/actionCreators";
+import {Article} from "./sass/components/Article";
 
-function App() {
+const App: React.FC = () => {
+  const articles: readonly IArticle[] = useSelector(
+      (state: ArticleState) => state.articles,
+      shallowEqual
+  )
+
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const saveArticle = React.useCallback(
+      (article: IArticle) => dispatch(addArticle(article)),
+      [dispatch]
+  )
   return (
-    <div className="App">
+    <main>
+      <h1>My articles</h1>
+      <AddArticle saveArticle={saveArticle} />
+        {articles.map((article: IArticle) => (
+            <Article key={article.id} article={article} removeArticle={removeArticle} />
+        ))}
 
-    </div>
+    </main>
   );
 }
 
